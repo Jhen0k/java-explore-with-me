7d33114de7c2f853a7979657f7fb8773a7e5cc57
Jhen0k <93070923+Jhen0k@users.noreply.github.com>
2024-01-22 00:24:01 +0300

    Update Stat.java

--- ewm-stats/server/src/main/java/ru/practicum/ewm/model/Stat.java
+++ ewm-stats/server/src/main/java/ru/practicum/ewm/model/Stat.java
@@ -2,7 +2,6 @@ package ru.practicum.ewm.model;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
