078fd3ce29ceb8544896b79459e8127ac2dc016c
Jhen0k <93070923+Jhen0k@users.noreply.github.com>
2024-01-23 16:50:46 +0300

    Update CategoryMapper.java

--- ewm-main/src/main/java/ru/practicum/ewm/category/mapper/CategoryMapper.java
+++ ewm-main/src/main/java/ru/practicum/ewm/category/mapper/CategoryMapper.java
@@ -2,7 +2,6 @@ package ru.practicum.ewm.category.mapper;
 
 import org.mapstruct.Mapper;
 import ru.practicum.ewm.category.dto.CategoryDto;
-import ru.practicum.ewm.category.dto.NewCategoryDto;
 import ru.practicum.ewm.model.Category;
 
 @Mapper(componentModel = "spring")
