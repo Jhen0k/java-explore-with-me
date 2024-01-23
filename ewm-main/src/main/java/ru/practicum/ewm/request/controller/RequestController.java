d1e48cf74719240726488dc3c460b7fb078d2cb6
Jhen0k <93070923+Jhen0k@users.noreply.github.com>
2024-01-23 16:48:13 +0300

    Update RequestController.java

--- ewm-main/src/main/java/ru/practicum/ewm/request/controller/RequestController.java
+++ ewm-main/src/main/java/ru/practicum/ewm/request/controller/RequestController.java
@@ -4,7 +4,6 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.ewm.request.dto.ParticipationRequestDto;
 import ru.practicum.ewm.request.service.RequestService;
 
-import javax.validation.constraints.Min;
 import java.util.List;
 
 @RestController
