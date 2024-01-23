56401f83ea97c897026f137bb662b99b3a15afe5
Jhen0k <93070923+Jhen0k@users.noreply.github.com>
2024-01-23 16:50:21 +0300

    Update CategoryPublicController.java

--- ewm-main/src/main/java/ru/practicum/ewm/category/controller/CategoryPublicController.java
+++ ewm-main/src/main/java/ru/practicum/ewm/category/controller/CategoryPublicController.java
@@ -4,7 +4,6 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
