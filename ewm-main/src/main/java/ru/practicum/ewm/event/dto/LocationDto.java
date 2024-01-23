2b8010d0d0b5116d8add9f5c274f93459234b79e
Jhen0k <93070923+Jhen0k@users.noreply.github.com>
2024-01-23 16:53:11 +0300

    Update LocationDto.java

--- ewm-main/src/main/java/ru/practicum/ewm/event/dto/LocationDto.java
+++ ewm-main/src/main/java/ru/practicum/ewm/event/dto/LocationDto.java
@@ -2,15 +2,10 @@ package ru.practicum.ewm.event.dto;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @Data
